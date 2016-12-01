2226fdc0a819610625751bc735f488f1ca23a919
Your Name <you@example.com>
2016-12-01 19:03:46 +0800

    update 20161201_190346_4

---
+++ 1.py
@@ -0,0 +1,10 @@
+name = raw_input('Please input your name: ')
+age = int(raw_input('Please input your age: '))
+
+if name == 'nimei' or age == 2:
+    print 'Hello nimei'
+    print 'xxxxxxx'
+else:
+    print 'You are not nimei'
+
+print 'not if in'
