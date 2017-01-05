b1ccda25aed7a37625e247dab2cb2f5455cc8ba1
Your Name <you@example.com>
2017-01-05 14:40:21 +0800

    update 20170105_144021_4

--- python/05/flask2.py
+++ python/05/flask2.py
@@ -1,5 +1,10 @@
 # coding:utf8
 
+
+## install flask module
+#pip install flask
+
+
 ## request 获取参数
 ## render_template 渲染模版文件,默认的模版文件在templates文件夹下面
 ## redirect 跳转
@@ -7,8 +12,10 @@ from flask import Flask,request,render_template,redirect
 import mem2
 import register
 
-## 新建app
+
+## new create app
 app = Flask(__name__)
+
 
 ## 监听一个url,路径是根目录
 ## The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.
@@ -34,35 +41,44 @@ def index():
     #return render_template('login.html')
     
     user_list = [['user','pwd'], ['fuck','nimei'], ['abc','123']]
-    return render_template('login.html', error_msg='xxx', ulist=user_list)
+    return render_template('login.html', error_msg='login.html', ulist=user_list)
+
+
+## google baidu browser search
+@app.route('/form.html')
+def form():
+    return render_template('form.html')
+
 
 ## 监听一个url,路径是/huoying
-@app.route('/huoying')
-
 ## 当这个路由被访问的时候,触发下面这个函数执行
+@app.route('/huoying')
 def huoying():
     ## 函数的返回值会显示在浏览器中
     #return 'huoying xxx123'
 
-    ## request
+    ## enter in the request
     ## http://192.168.200.8:9092/huoying
     ## http://192.168.200.8:9092/huoying?username=fuck&password=123
     print request.args
 
-    ## get方法获取url参数
+    ## request.args的get方法获取url参数
     user = request.args.get('username')
     pwd = request.args.get('password')
     return 'huoying %s:%s' % (user, pwd)
 
+
 ## 练习浏览器中显示内存信息,导入模块mem2
-@app.route('/mem')
-def memxx():
+@app.route('/meminfo')
+def mem():
     return mem2.getMem()
+
 
 ## 定义统一的错误页面
 @app.route('/error')
 def error():
     return '<img width="100%" src="http://www.laoshiz.com/uploads/allimg/140218/2136352614-2.jpg">'
+
 
 ## 练习浏览器输入用户名和密码,点击提交
 ## 后端的user.txt文件中判断是不是有权限登录
