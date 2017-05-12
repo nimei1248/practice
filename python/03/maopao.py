d0c6bab508e9c03c11c5dd4a407a9428323c5c82
Jason Bourne <JasonBourne@Jason-Broune.local>
2017-05-12 20:37:19 +0800

    update 20170512_203719_5

--- python/03/maopao.py
+++
@@ -1,9 +0,0 @@
-l2 = [1,3,2,6,8,1,20,3,40]
-
-for i in range(len(l2)-1,0,-1):
-    for j in range(i):
-        if l2[j] > l2[j + 1]:
-            tmp = l2[j]
-            l2[j] = l2[j + 1]
-            l2[j + 1] = tmp
-print l2
--- python/10/1.py
+++
@@ -1,9 +0,0 @@
-def hello(name):
-    print 'hello ' + name
-
-def my_hello(fn,name):
-    print 'hello world'
-    fn(name)
-    print 'hello python'
-
-my_hello(hello,'nimei')
