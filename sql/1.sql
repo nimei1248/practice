dad79112dd7f7fbcdaad3bac50525dd42f507509
Your Name <you@example.com>
2017-06-08 18:35:28 +0800

    update 20170608_183528_4

---
+++ sql/1.sql
@@ -0,0 +1,35 @@
+show processlist:
+
+state:
+
+Waiting on empty queue                                                     
+
+Waiting for master to send event                                           
+
+Slave has read all relay log; waiting for the slave I/O thread to update it
+
+Waiting for an event from Coordinator                                      
+
+Reading from net                                                           
+
+Master has sent all binlog to slave; waiting for binlog to be updated
+
+Waiting for semi-sync ACK from slave
+
+init
+
+updating
+
+Sending data
+
+cleaning up
+
+freeing items
+
+Writing to net
+
+Reading from net
+
+Searching rows for update
+
+
