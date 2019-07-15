08ef8e1943d0ce4bac6f518ff32ef0c190c0e3c1
Your Name <you@example.com>
2019-07-15 14:34:35 +0800

    update 20190715_143435_1

--- sql/tmp2/sql_modify_before_20190715.sql
+++ sql/tmp2/sql_modify_before_20190715.sql
@@ -51,3 +51,25 @@ SET
 WHERE
     product_id = 'bike'
         AND customer_id = 199707010701
+		
+
+第3部分查询比较慢thr_count:
+SQL> show warnings\G
+*************************** 1. row ***************************
+  Level: Note
+   Code: 1003
+Message: /* select#1 */ 
+select count(1) AS `thr_count` 
+from 
+    `db1`.`same_a_table` `t7` 
+	join `db1`.`same_a_table` `t6` 
+	join `db1`.`same_a_table` `t5` 
+where 
+    ((`db1`.`t5`.`parent_id` = `db1`.`t6`.`customer_id`) 
+	and (`db1`.`t7`.`customer_id` = `db1`.`t6`.`parent_id`) 
+	and (`db1`.`t7`.`parent_id` = 199707010701) 
+	and (`db1`.`t5`.`product_id` = 'bike') 
+	and (`db1`.`t6`.`product_id` = 'bike') 
+	and (`db1`.`t7`.`product_id` = 'bike'))
+1 row in set (0.00 sec)
+
